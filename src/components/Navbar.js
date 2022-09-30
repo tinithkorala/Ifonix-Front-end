f652de5de0fd0cebbf10bd10f815258fcc869eaf
tinithk <tinithse@gmail.com>
2022-09-30 12:11:02 +0530

    Nabar component response error fixed swal error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Navbar = ({handleAuthStatus, isAuthenticated, userType}) => {
 
