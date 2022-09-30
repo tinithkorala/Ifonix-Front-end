a46acd3f510e5464f0785a3759fcd8eb9403dff6
tinithk <tinithse@gmail.com>
2022-09-30 12:51:11 +0530

    add btn for navigate laravel documentaion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,13 @@
+import axios from "axios";
+
 const Home = () => {
+
+    const handleClick = () => {
+
+        window.open(axios.defaults.baseURL+'api/documentation');
+
+    }
+
     return ( 
         <div className="home">
 
@@ -7,8 +16,10 @@ const Home = () => {
                 <h1 className="display-3 mb-0"> Hi <span className="fw-bold text-capitalize">{localStorage.getItem('auth_name')}, </span> </h1>
                 <h1 className="display-1 my-0">Welcome to ABC</h1>
                 { !localStorage.getItem('auth_name') && <p className="lead">Currently you are not logged in...</p> }
-        
+
+
             </div>
+            <button className="btn bg-swagger mt-5 fw-bold" onClick={handleClick} >Laravel Swagger API Documentation</button>
             
         </div>
     );
